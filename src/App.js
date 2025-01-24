22eded64e51ae7fe0e127193b2696f573ded6784
Mahmoud Elzeny <mahmoudelziny20@gmail.com>
2025-01-24 11:35:36 +0200

    sore the watched data in local storage

--- src/App.js
+++ src/App.js
@@ -9,11 +9,15 @@ const KEY = "d4a10570";
 export default function App() {
 
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [query, setQuery] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+  const [watched, setWatched] = useState(function () {
+    const storedWatched = localStorage.getItem("watched");
+    return storedWatched ? JSON.parse(storedWatched) : [];
+  });
+
 
   function handleSelectMovie(id) {
     setSelectedId(selectedId => (id === selectedId ? null : id));
@@ -30,6 +34,10 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched(watched => watched.filter((movie) => movie.imdbID !== id));
   }
+
+  useEffect(function () {
+        localStorage.setItem("watched", JSON.stringify(watched));
+  }, [watched]);
 
   useEffect(function () {
 
