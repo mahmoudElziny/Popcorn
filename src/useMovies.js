4374b0553025a192afd9ad9c1815028a4fec322d
Mahmoud Elzeny <mahmoudelziny20@gmail.com>
2025-01-25 07:28:15 +0200

    https

--- src/useMovies.js
+++ src/useMovies.js
@@ -18,7 +18,7 @@ export function useMovies(query) {
                 setIsLoading(true);
                 setError("");
 
-                const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`, { signal: controller.signal });
+                const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`, { signal: controller.signal });
 
                 if (!res.ok) throw new Error("Something went wrong wih fetching movies");
 
